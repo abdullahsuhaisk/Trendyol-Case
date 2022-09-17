628748261ad815bcc42ae1e6e05f87dd341e03b5
Abdullah Süha ışık <abdullahsuhaisik@gmail.com>
2022-09-17 15:55:17 +0300

    add some validation

--- frontEnd/feedbackyfe/src/Components/FeedbackButton/FeedBackButton.tsx
+++ frontEnd/feedbackyfe/src/Components/FeedbackButton/FeedBackButton.tsx
@@ -3,9 +3,11 @@ import { ModalProp } from '../../Types/Modal.types';
 
 function FeedBackButton({ show, setShow }: ModalProp) {
   return (
-    !show ? (<span role={"button"} onClick={() => { setShow(!show) }} className="">
-      <MdRateReview className="feedbacky-open-icon-button" />
-    </span>) : null
+    !show ?
+      <span role={"button"} onClick={() => { setShow(!show) }} className="">
+        <MdRateReview className="feedbacky-open-icon-button" />
+      </span> :
+      null
   )
 }
 export default FeedBackButton;
--- frontEnd/feedbackyfe/src/Components/Modal/Modal.tsx
+++ frontEnd/feedbackyfe/src/Components/Modal/Modal.tsx
@@ -5,36 +5,75 @@ import './Modal.css';
 
 function Modal({ setShow, show }: ModalProp) {
   const [usersFeedback, setUsersFeedback] = useState('');
+  const [feedBackSended, setFeedBackSended] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  function handleOnChangeText(e: React.ChangeEvent<HTMLTextAreaElement>):void {
+  function resetAllState() {
+    setLoading(false);
+    setShow(false);
+    setFeedBackSended(false)
+  }
+
+  function textAreaValidation(): boolean {
+    if (!usersFeedback) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function handleOnChangeText(e: React.ChangeEvent<HTMLTextAreaElement>): void {
     setUsersFeedback(e.target.value)
   }
 
+  function mockSendFeedBack() {
+    setLoading(true)
+    setTimeout(() => {
+      setFeedBackSended(true);
+      setLoading(false);
+    }, 1000);
+  }
+
+  function sendUserFeedBack() {
+    if (textAreaValidation()) {
+      mockSendFeedBack();
+    } else {
+      alert('Error');
+    }
+  }
   return (
     show ?
       <div className='feedbacky-modal'>
-        {/* <div className='feedbacky-modal-wrapper'> */}
         <div className="feedbacky-modal-container">
           <div className="feedbacky-modal-header feedbacky-flexEnd">
-            <span onClick={() => { setShow(false) }} className="feedbacky-modal-cancelButton" role={'button'} data-testid="cancel" >
+            <span onClick={() => { resetAllState() }} className="feedbacky-modal-cancelButton" role={'button'} data-testid="cancel" >
               <MdClose />
             </span>
           </div>
-          <div className="feedbacky-modal-header feedbacky-flex">
-            <h1>
-            SEND YOUR FEEDBACK
-            </h1>
-          </div>
-          <div className="feedbacky-modal-textbox feedbacky-flex">
-            <textarea className='feedbacky-fullWidth' onChange={(e) => handleOnChangeText(e)} placeholder="Please write your comments..."/>
-          </div>
-          <div className="feedbacky-modal-sendButton feedbacky-flex">
-            <button className='feedbacky-fullWidth' onClick={() => {console.log(usersFeedback)}}>
-              Send
-            </button>
-          </div>
+          {feedBackSended ?
+            <div className='feedbacky-flex feedbacky-feedback-recieved'>
+              <h1>
+                WE HAVE GOT <br /> YOUR FEEDBACK
+              </h1>
+            </div> :
+            <>
+              <div className="feedbacky-modal-header feedbacky-flex">
+                <h1>
+                  SEND YOUR FEEDBACK
+                </h1>
+              </div>
+              <div className="feedbacky-modal-textbox feedbacky-flex">
+                <textarea className='feedbacky-fullWidth' onChange={(e) => handleOnChangeText(e)} placeholder="Please write your comments..." />
+              </div>
+              <div className="feedbacky-modal-sendButton feedbacky-flex">
+                <button className='feedbacky-fullWidth' onClick={() => { sendUserFeedBack() }}>
+                  Send
+                </button>
+              </div>
+            </>
+          }
         </div>
-        {/* </div> */}
+
 
       </div> : null
   );
